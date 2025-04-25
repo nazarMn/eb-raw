2621d09ec72a353d506fcca7d297c7ffbad48e57
MarkivNazar <nazarmn2008@gmail.com>
2025-04-25 14:08:21 +0300

    OurFeaturedProducts page stylized

--- components/OurFeaturedProducts/OurFeaturedProducts.jsx
+++ components/OurFeaturedProducts/OurFeaturedProducts.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack'
 
 export default function OurFeaturedProducts() {
   return (
@@ -32,10 +34,55 @@ export default function OurFeaturedProducts() {
 
         </div>
 
+        <div className="w-full h-[80%] flex justify-around items-start flex-wrap p-10">
 
-        <div>
-            <Image></Image>
-        </div>
+
+
+        <div className="w-[370px] h-[461px] bg-white flex flex-col shadow-md overflow-hidden">
+  <Image
+    src="/sliderImg1.png"
+    alt="OurFeaturedProducts-Img1"
+    width={370}
+    height={700}
+    className="w-full h-[75%] object-cover"
+  />
+
+  <div className="w-full h-[25%] flex justify-between items-start p-3">
+    <div className="w-1/2 h-full flex flex-col justify-between">
+      <h2 className="text-black text-[18px] font-medium">Man Sweet Hoodie</h2>
+      <button className="w-[130px] h-[40px] border border-[#333] text-black text-[15px] font-medium bg-white hover:bg-gray-100 transition">
+        Add to cart
+      </button>
+    </div>
+
+    <div className="flex flex-col items-end justify-between gap-4">
+
+
+<div class="flex items-center gap-2">
+<Stack spacing={1}>
+      <Rating name="half-rating" defaultValue={2.5} precision={0.5}  readOnly sx={{
+      color: '#023047', 
+      '& .MuiRating-iconEmpty': {
+        color: '#cfd8dc', 
+      },
+    }} />
+    
+    </Stack>
+<p>(99+)</p>
+</div>
+
+        
+      <h2 className="text-black text-[18px] font-medium">$150.00</h2>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+</div>
+
 
     </div>
   )
