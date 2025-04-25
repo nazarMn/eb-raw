2e9e441548241b0d5f3446ed04d4f0758a213f0d
MarkivNazar <nazarmn2008@gmail.com>
2025-04-25 15:16:14 +0300

    font connected

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import { Jost } from "next/font/google";
 import Header from "../components/Header/Header";
 import MenuBar from "../components/MenuBar/MenuBar";
 import Hero from "../components/Hero/Hero";
@@ -5,29 +6,21 @@ import ShopPolicy from "../components/ShopPolicy/ShopPolicy";
 import NewArrival from "../components/NewArrival/NewArrival";
 import OurFeaturedProducts from "@/components/OurFeaturedProducts/OurFeaturedProducts";
 
+const jost = Jost({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function Home() {
-
-  
   return (
-<>
-
-<Header />
-<div className="h-[1px] bg-white shadow-[0px_1px_1px_rgba(0,0,0,0.2)]"></div>
-
-
-<MenuBar />
-
-<Hero />
-
-<ShopPolicy />
-
-<NewArrival />
-
-<OurFeaturedProducts />
-
-</>
- 
-
+    <div className={jost.className}>
+      <Header />
+      <div className="h-[1px] bg-white shadow-[0px_1px_1px_rgba(0,0,0,0.2)]"></div>
+      <MenuBar />
+      <Hero />
+      <ShopPolicy />
+      <NewArrival />
+      <OurFeaturedProducts />
+    </div>
   );
 }
