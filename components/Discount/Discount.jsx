f7cec9ea857f39af98e77435c694fd5bf9662c96
MarkivNazar <nazarmn2008@gmail.com>
2025-04-25 16:06:05 +0300

    added adaptive to Discount page

--- components/Discount/Discount.jsx
+++ components/Discount/Discount.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 
 export default function Discount() {
   return (
-    <div className="relative w-full h-[300px] bg-cover bg-center" style={{ backgroundImage: "url('/DiscountBg.png')" }}>
-  <div className="absolute w-full h-full bg-[#023047] opacity-95">
+    <div
+      className="relative w-full h-[300px] bg-cover bg-center"
+      style={{ backgroundImage: "url('/DiscountBg.png')" }}
+    >
+      <div className="absolute w-full h-full bg-[#023047] opacity-95" />
+      <div className="absolute w-full h-full flex justify-center items-center px-4">
+        <div className="w-full max-w-[970px] flex flex-wrap justify-center sm:justify-evenly items-center gap-4 sm:gap-2 bg-[#E5F6FF] px-4 py-6 sm:py-4">
+          <h2 className="text-[#333333] text-[18px] sm:text-[24px] font-medium text-center sm:text-left">
+            Super discount for your first purchase
+          </h2>
 
-  </div>
-  <div className="absolute w-full h-full flex justify-center items-center">
+          <button className="w-full sm:w-[170px] h-[50px] border border-dashed border-[#023047] text-[#023047] font-medium text-[16px] text-center rounded">
+            FREE10DELIVERY
+          </button>
 
-    <div className='w-[970px] h-[100px] flex justify-evenly items-center bg-[#E5F6FF]'>
-        <h2 className='text-[#333333] text-[24px] font-medium'>Super discount for your first purchase</h2>
-
-        <button className='w-[170px] h-[50px] border border-dashed border-[#023047] text-[#023047] font-medium text-[16px] text-center'>FREE10DELIVERY</button>
-
-        <h2 className='text-[#333333] text-[18px] font-medium'>Use discount code in checkout!</h2>
-
-        
-
+          <h2 className="text-[#333333] text-[16px] sm:text-[18px] font-medium text-center sm:text-left">
+            Use discount code in checkout!
+          </h2>
+        </div>
+      </div>
     </div>
-   
-  </div>
- 
-</div>
-
-  )
+  );
 }
