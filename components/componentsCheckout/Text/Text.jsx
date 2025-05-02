2d784dbb5038b859373eb83e575790d6daf46768
MarkivNazar <nazarmn2008@gmail.com>
2025-05-02 17:47:40 +0300

    added text to Checkout

---
+++ components/componentsCheckout/Text/Text.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Text() {
+  return (
+    <div className="flex flex-col gap-2 mt-10 px-4 sm:px-6 md:px-10 lg:px-20">
+
+      <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold">
+        Placing an order
+      </h2>
+
+    </div>
+  )
+}
--- pages/Checkout.jsx
+++ pages/Checkout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Jost } from "next/font/google";
 import Header from '@/components/componentsCheckout/Header/Header';
+import Text from '@/components/componentsCheckout/Text/Text';
 
 
 const jost = Jost({
@@ -10,8 +11,9 @@ const jost = Jost({
 export default function Checkout() {
     
   return (
-    <div className={jost.className} >
+    <div className={jost.className}>
       <Header />
+      <Text />
       
     </div>
   )
