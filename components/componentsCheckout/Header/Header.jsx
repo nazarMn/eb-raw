349a7d837c78eafaa882c791857f7e468e63460e
MarkivNazar <nazarmn2008@gmail.com>
2025-05-02 17:40:03 +0300

    added header to Checkout page

---
+++ components/componentsCheckout/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Header() {
+  return (
+   <header className="w-full h-[20%] ">
+         <div className="flex flex-col md:flex-row items-center justify-between gap-4 md:gap-0 px-4 md:px-10 lg:px-[135px] py-4 border-b border-[#E5E5E5] ">
+           
+         
+           <div className="flex-shrink-0 pr-4">
+             <Image src="/Logo.svg" alt="Logo" width={130} height={27.36} priority />
+           </div>
+
+
+   
+          
+           <div className="flex items-center gap-5 pl-1 sm:pl-4 mt-2 md:mt-0">
+                {["Profile"].map((icon, i) => (
+                  <div key={i} className="relative">
+                    <Image
+                      src={`/${icon}.svg`}
+                      alt={icon}
+                      width={26}
+                      height={25}
+                      className="cursor-pointer"
+                    />
+                   
+                  </div>
+                ))}
+               
+              </div>
+   
+     
+          
+         </div>
+       </header>
+  )
+}
--- pages/Checkout.jsx
+++ pages/Checkout.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Jost } from "next/font/google";
+import Header from '@/components/componentsCheckout/Header/Header';
 
+
+const jost = Jost({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  });
 export default function Checkout() {
-    const jost = Jost({
-      subsets: ["latin"],
-      weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-    });
     
   return (
-    <div className={jost.className}>
+    <div className={jost.className} >
+      <Header />
       
     </div>
   )
