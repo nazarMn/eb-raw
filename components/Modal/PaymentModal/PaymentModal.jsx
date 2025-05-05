1572df826cf2424758c49acdf0853cc0c2ab8635
MarkivNazar <nazarmn2008@gmail.com>
2025-05-05 22:17:53 +0300

    fixe

--- components/Modal/PaymentModal/PaymentModal.jsx
+++ components/Modal/PaymentModal/PaymentModal.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Modal from 'react-modal';
 
 export default function PaymentModal() {
   return (
-    <div></div>
+     <Modal
+          isOpen={true}
+
+          className="bg-white rounded-lg w-full max-w-2xl mx-auto mt-20 p-4 sm:p-6 shadow-xl outline-none"
+          overlayClassName="fixed inset-0 bg-opacity-10 flex justify-center items-start z-50 px-2 shadow-xl"
+        >
+
+        </Modal>
   )
 }
