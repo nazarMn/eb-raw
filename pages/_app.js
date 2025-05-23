487d776d92bad27172e19db09dc6e7018f797abe
MarkivNazar <nazarmn2008@gmail.com>
2025-05-23 21:34:17 +0300

    fixe

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import "@/styles/globals.css";
-import '../components/componentsHome/Hero/Hero.css';
-import '../components/componentsHome/Review/Review.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
