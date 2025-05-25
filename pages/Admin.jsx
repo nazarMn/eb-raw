f031dbf7927652e6385d99f10624096864af104e
MarkivNazar <nazarmn2008@gmail.com>
2025-05-25 21:56:26 +0300

    connected jost font to admin panel

--- pages/Admin.jsx
+++ pages/Admin.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Jost } from "next/font/google";
+
+
+const jost = Jost({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
 
 export default function Admin() {
   return (
-    <div>
+      <div className={jost.className}>
       
     </div>
   )
